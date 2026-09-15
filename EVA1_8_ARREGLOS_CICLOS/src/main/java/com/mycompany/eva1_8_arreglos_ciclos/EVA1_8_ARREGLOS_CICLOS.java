e4f55ab6adbd6a535d51e1cbe11fe30d7135e039
erickdelarosanunez <ee834190@gmail.com>
2026-09-14 23:18:51 -0600

    Add files via upload

---
+++ EVA1_8_ARREGLOS_CICLOS/src/main/java/com/mycompany/eva1_8_arreglos_ciclos/EVA1_8_ARREGLOS_CICLOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_arreglos_ciclos;
+
+/**
+ *
+ * @author chanc
+ */
+public class EVA1_8_ARREGLOS_CICLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ EVA1_9_MATRICES/src/main/java/com/mycompany/eva1_9_matrices/EVA1_9_MATRICES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_matrices;
+
+/**
+ *
+ * @author chanc
+ */
+public class EVA1_9_MATRICES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
